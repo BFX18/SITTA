8d6ed01d6758d1cf6809850adc158ccdc3ce51ab
Guntur <syekh.guntur10@gmail.com>
2026-05-05 19:44:34 +0700

    fix: main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+// @ts-ignore: CSS module declaration not available in this project setup
+import "./index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
